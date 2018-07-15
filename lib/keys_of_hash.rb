f299f882d71707a937d8116ac6a142000458eab3
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-15 09:05:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,8 @@
 class Hash
   def keys_of(*arguments)
     # Returns an Array
-    puts "arguments = #{arguments}"
     new_array = []
     self.each do |key, value|
-      puts "key = #{key}, value = #{value}"
       arguments.each do |element|
         if value == element
           new_array.push(key)
