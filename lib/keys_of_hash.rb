dd72faf287d4947cc23d399c9bbb0e63075ad5f5
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-15 09:06:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,6 @@
 class Hash
   def keys_of(*arguments)
+    #Method keys_of takes an undefined number of arguments, using the splat operator, and returns an array with every
     # Returns an Array
     new_array = []
     self.each do |key, value|
